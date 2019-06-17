b8e9a34a01c2ad6398abb97cc9d39128fa43f397
susanlin0705 <susan@Meng-hans-MacBook-Pro.local>
2019-06-17 17:00:46 -0500

    intro testing

--- code.js
+++ code.js
@@ -2,3 +2,16 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+function add(a,b) {
+    if (typeof a==="string" ||typeof b==="string"){
+        return false;
+    }else{
+        return a+b;
+    }
+}
+
+function sayHello(name) {
+    return "say hello"
+
+}
